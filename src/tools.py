aa44c66769a96f4566eae1ab3e7fced4116d1eac
amar-at-iitm <ma24m002@smail.iitm.ac.in>
2026-08-31 20:17:11 +0530

    Updated for cifar10

--- src/tools.py
+++ src/tools.py
@@ -1,20 +1,41 @@
 from torchvision import transforms
 
+CIFAR10_MEAN = (0.4914, 0.4822, 0.4465)
+CIFAR10_STD = (0.2470, 0.2435, 0.2616)
 
-def get_transforms(augmentation):
-    if augmentation:
-        train_transform = transforms.Compose([
-            transforms.RandomHorizontalFlip(),
-            transforms.RandomRotation(10),
-            transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
+
+def get_transforms(augmentation, dataset="cifar10"):
+    if dataset.lower() == "cifar10":
+        if augmentation:
+            train_transform = transforms.Compose([
+                transforms.RandomCrop(32, padding=4),
+                transforms.RandomHorizontalFlip(),
+                transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1),
+                transforms.ToTensor(),
+                transforms.Normalize(CIFAR10_MEAN, CIFAR10_STD),
+            ])
+        else:
+            train_transform = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize(CIFAR10_MEAN, CIFAR10_STD),
+            ])
+        val_transform = transforms.Compose([
             transforms.ToTensor(),
+            transforms.Normalize(CIFAR10_MEAN, CIFAR10_STD),
         ])
     else:
-        train_transform = transforms.Compose([
+        if augmentation:
+            train_transform = transforms.Compose([
+                transforms.RandomHorizontalFlip(),
+                transforms.RandomRotation(10),
+                transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
+                transforms.ToTensor(),
+            ])
+        else:
+            train_transform = transforms.Compose([
+                transforms.ToTensor(),
+            ])
+        val_transform = transforms.Compose([
             transforms.ToTensor(),
         ])
-    
-    val_transform = transforms.Compose([
-        transforms.ToTensor(),
-    ])
     return train_transform, val_transform
